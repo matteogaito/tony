c748b97662aa39578e96f4a8ecf1b7a0ce7d7be7
Matteo Gaito <matteo.gaito@dada.eu>
2018-04-04 10:12:05 +0200

    render drive directory

--- bootstrap_tony.vagrant.sh
+++ bootstrap_tony.vagrant.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 apt-get update
-apt-get install git vim python-virtualenv python-pip python3-pip libssl-dev -y
+apt-get install git vim python-virtualenv python-pip python3-virtualenv python3-pip libssl-dev -y
 sudo locale-gen "it_IT.UTF-8"
 
-pip3 install -r /vagrant/tony_code/requirements.txt
+pip3 install -r /vagrant/code/requirements.txt
 
 apt-get install sqlite3 -y
--- code/app/home.py
+++ code/app/home.py
@@ -1,13 +1,24 @@
 # -*- coding: utf-8 -*-
 
 import flask
-from flask_gapps_connector import *
+import werkzeug
+import httplib2
+
+import flask_gapps_connector
+
+from apiclient import discovery
+from flask import render_template
+
+from app import app
 
 def getDriveTree():
     """ Show Google Drive Tree
     """
     drive_scope = 'https://www.googleapis.com/auth/drive'
-    credentials = gapi_connector.get_credentials(scope=drive_scope)
+    google_access_file = app.config['GOOGLE_API_ACCESS']
+    credentials = flask_gapps_connector.get_credentials(google_access_file=google_access_file, scope=drive_scope)
+
+    print(credentials)
 
     if credentials:
         http = credentials.authorize(httplib2.Http())
@@ -19,10 +30,14 @@ def getDriveTree():
         items = results.get('files', [])
         return items
     else:
-        return flask.redirect(flask.url_for('oauth2callback',gapi_scope=drive_scope))
+        return flask.redirect(flask.url_for('oauth2callback',gapi_scope=drive_scope,google_access_file=google_access_file))
 
 @app.route("/")
 def home():
   mydrivedirs = getDriveTree()
+  #Do redirect if the response of the function is a redirect
   if isinstance(mydrivedirs, werkzeug.wrappers.Response):
-      return mydrivedirs
+    return(mydrivedirs)
+  #return render_template('home.html', mydrivedirs=mydrivedirs, mytenevents=mytenevents)
+  return render_template('home.html', mydrivedirs=mydrivedirs)
+  return("ok")

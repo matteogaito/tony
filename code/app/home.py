4ebb1e35fe0bb261c126fe333d95d566212ccc53
Matteo Gaito <matteo.gaito@gmail.com>
2018-04-21 17:58:30 +0200

    added path for develop module and clean code

--- code/app/home.py
+++ code/app/home.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
 
-#from app.flask_gapps_connector import DriveInizialize
-#from app.flask_gapps_connector.flask_gapps_connector import *
+#import sys
+#sys.path.insert(0, '/vagrant/flask-gapps-connector')
+
 from flask_gapps_connector import DriveInizialize
 
 import werkzeug
@@ -12,12 +13,9 @@ from app import app
 @app.route("/")
 def home():
     drive = DriveInizialize( app.config['DRIVE_SCOPE'], app.config['GOOGLE_API_ACCESS'])
+    #Do redirect if the response of the function is a redirect
     if isinstance(drive, werkzeug.wrappers.Response):
         return(drive)
     query = "'root' in parents and mimeType = 'application/vnd.google-apps.folder'"
     mydrivedirs = drive.ListFile({ 'q': query }).GetList()
-    #Do redirect if the response of the function is a redirect
-    if isinstance(mydrivedirs, werkzeug.wrappers.Response):
-        return(mydrivedirs)
-    #return render_template('home.html', mydrivedirs=mydrivedirs, mytenevents=mytenevents)
     return render_template('home.html', mydrivedirs=mydrivedirs)

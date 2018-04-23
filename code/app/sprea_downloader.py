330153691378d292e722d3fd3d872b52b52ccffb
Matteo Gaito <matteo.gaito@gmail.com>
2018-04-23 12:24:33 +0200

    fixed google drive directory

--- code/app/sprea_downloader.py
+++ code/app/sprea_downloader.py
@@ -8,23 +8,26 @@ from app.telegram_connector import botSendMessage
 # sl object contain logged session in sprea.it
 sl = Sprea(app.config['SPREA_USERNAME'], app.config['SPREA_PASSWORD'])
 
+
 @app.route('/sprea/check_last_pdf')
 def checkEsistenceLastPDFintoGdrive():
-    #Get Pdf Name by Url
+    # Get Pdf Name by Url
     last_pdf_url = sl.getOnePdfUrlofCampaign(app.config['SPREA_CAMPAIGN'], 0)
     pdfinfo = sl._getPdfInfo(last_pdf_url)
 
-    #Check into Google Drive
-    drive = DriveInizialize( app.config['DRIVE_SCOPE'], app.config['GOOGLE_API_ACCESS'])
-    file_list = drive.ListFile({'q': "'0B_9s9T8tAnUWTWNUbDZVbVBjOFE' in parents and trashed=false"}).GetList()
+    # Check into Google Drive
+    drive = DriveInizialize(app.config['DRIVE_SCOPE'], app.config['GOOGLE_API_ACCESS'])
+    q = "'" + app.config['EBOOK_FOLDER'] + "' in parents and trashed=false"
+    file_list = drive.ListFile({'q': q}).GetList()
 
-    #Loop into file list in order to found pdf
-    found=False
+    # Loop into file list in order to found pdf
+    found = False
     for f in file_list:
         if f['title'] == pdfinfo['name']:
-            found=True
+            found = True
 
     return(last_pdf_url, found)
+
 
 @app.route('/download_last_pdf_if_missing')
 def downloadLastEbookIfMissing():
@@ -32,13 +35,12 @@ def downloadLastEbookIfMissing():
     pdfinfo = sl._getPdfInfo(last_pdf_url)
     if not found:
         pdf_path = sl.downloadPDFbyURL(last_pdf_url)
-        drive = DriveInizialize( app.config['DRIVE_SCOPE'], app.config['GOOGLE_API_ACCESS'])
+        drive = DriveInizialize(app.config['DRIVE_SCOPE'], app.config['GOOGLE_API_ACCESS'])
         f = drive.CreateFile({'title': pdfinfo['name'], "parents": [{"kind": "drive#fileLink", "id": app.config['EBOOK_FOLDER']}]})
         f.SetContentFile(pdf_path)
         f.Upload()
-        botSendMessage('File {} downloaded'.format(pdfinfo['name']))
+        botSendMessage('File "{}" downloaded with name '.format(pdfinfo['title'], pdfinfo['name']))
         return(pdf_path)
     else:
-        botSendMessage('File {} already downloaded'.format(pdfinfo['name']))
+        botSendMessage('File "{}" already downloaded'.format(pdfinfo['title']))
         return("Pdf Presente")
-
